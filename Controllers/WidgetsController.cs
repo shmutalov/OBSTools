38e0cc661fef3d137e13832d329bf6bf1391b748
Sherzod Mutalov <shmutalov@gmail.com>
2022-04-23 11:57:40 +0500

    Added current song widget

---
+++ Controllers/WidgetsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OBSTools.Controllers
+{
+    public class WidgetsController : Controller
+    {
+        public IActionResult CurrentSong()
+        {
+            return View();
+        }
+    }
+}
